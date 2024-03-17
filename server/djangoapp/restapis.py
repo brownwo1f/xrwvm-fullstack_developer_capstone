457a2f80c60782b1dbef120137fb9f4785a77dc2
Deepak Chauhan <98384780+brownwo1f@users.noreply.github.com>
2024-03-18 04:18:03 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -81,7 +81,7 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception:
         print("Network exception occurred")
 
     def __str__(self):
