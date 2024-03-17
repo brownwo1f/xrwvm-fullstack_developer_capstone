8a3dd3808093f5b07383ba46217160f9cd56d19d
Deepak Chauhan <98384780+brownwo1f@users.noreply.github.com>
2024-03-18 04:00:04 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,57 +1,123 @@
+# Uncomment the following imports before adding the Model code
+
+
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# Create your models here.
+
+# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# - Name
+# - Description
+# - Any other fields you would like to include in car make model
+# - __str__ method to print a car make object
+
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Other fields as needed
+
+
 # Uncomment the imports below before you add the function code
+
 import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if (kwargs):
+    if kwargs:
         for key, value in kwargs.items():
-            params = params+key+"="+value+"&"
-
-    request_url = backend_url+endpoint+"?"+params
+            params = params + key + "=" + value + "&"
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
+
         response = requests.get(request_url)
         return response.json()
     except Exception:
         # If any error occurs
+
         print("Network exception occurred")
 
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
+
+
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
+
         response = requests.get(request_url)
         return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 # def post_review(data_dict):
 # Add code for posting review
+
+
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
         print("Network exception occurred")
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
+
+
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# - Many-To-One relationship to Car Make model (One Car Make has many
+# Car Models, using ForeignKey field)
+# - Name
+# - Type (CharField with a choices argument to provide limited choices
+# such as Sedan, SUV, WAGON, etc.)
+# - Year (IntegerField) with min value 2015 and max value 2023
+# - Any other fields you would like to include in car model
+# - __str__ method to print a car make object
+
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE
+    )  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
+    # Other fields as needed
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
