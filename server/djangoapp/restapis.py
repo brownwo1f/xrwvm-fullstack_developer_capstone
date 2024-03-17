9fe02fc889a08463f66880393dfb7446f6087371
Deepak Chauhan <deepak858783@gmail..com>
2024-03-17 17:51:41 -0400

    finished week 4 tasks dynamic pages

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -16,9 +16,9 @@ sentiment_analyzer_url = os.getenv(
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -27,7 +27,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception:
         # If any error occurs
         print("Network exception occurred")
 
